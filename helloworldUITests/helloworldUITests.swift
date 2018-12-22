265374fd71acffe70df0ff13edec2691e03f6a68
Tai <letientai299@gmail.com>
2018-12-23 05:15:18 +0800

    Add fastlane

--- helloworldUITests/helloworldUITests.swift
+++ helloworldUITests/helloworldUITests.swift
@@ -16,9 +16,9 @@ class helloworldUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
@@ -26,9 +26,8 @@ class helloworldUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testOpening() {
+        snapshot("main-page")
     }
 
 }
